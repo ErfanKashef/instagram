e676567adc3f11617a56d3f84518045e69f415cb
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-09-13 13:55:24 +0330

    add coponents profile

--- src/components/page/profile.jsx
+++ src/components/page/profile.jsx
@@ -19,7 +19,7 @@ const Profile = () => {
               <p className="text-sm">11</p>
             </div>
             <div className="flex gap-2 items-center">
-              <p className="tex">follower</p>
+              <p className="text-sm">follower</p>
               <p className="text-sm">11</p>
             </div>
             <div className="flex gap-2 items-center">
@@ -31,9 +31,19 @@ const Profile = () => {
           <div className="felx flex-col gap-2">
             <p className="text-sm">Upvox</p>
             <p className="text-sm text-gray-500">Product/service</p>
-            <p className="text-sm">@username</p>
+            <p className="text-sm">
+              Your favourite fun clips 🎦 in your language 🌎
+            </p>
             <p className="text-sm text-blue-700 cursor-pointer">Follow</p>
           </div>
+        </div>
+      </div>
+      <div className="bg-[#DBDBDB] rounded-full h-0.5 mb-6"></div>
+      <div className="flex justify-center">
+        <div className="grid  grid-cols-3 gap-3">
+          <img src="/public/img/photo.png" alt="photo" />
+          <img src="/public/img/photo.png" alt="photo" />
+          <img src="/public/img/photo.png" alt="photo" />
         </div>
       </div>
     </div>
