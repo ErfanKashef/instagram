4909d15368d2492cc6011db33104d6a35968f4d1
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-13 10:06:17 +0330

    ✨ insta logo added

--- src/InstaTitle.jsx
+++ src/InstaTitle.jsx
@@ -1,7 +1,7 @@
-export function InstaTitle() {
+export function InstaTitle({LogoWidth, LogoHeight}) {
     return (
     <div>
-        
+        <img src="./assets/instaLogo.png" alt="IG Logo" className="w-[{LogoWidth}] h-[{LogoHeight}]"/>
     </div>
 )
 }
