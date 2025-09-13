ceb258bfe82825bd4a62cb1360f08cb89f5fed82
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-13 10:40:47 +0330

    ✨ Login added some features

--- src/logIn.jsx
+++ src/logIn.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
 import { InstaTitle } from "./InstaTitle";
 
 export function LogIn() {
     return (
     <div>
         <InstaTitle LogoWidth="255px" LogoHeight="87px"/>
+        <span>
+            <p>
+            Don’t have an account?
+            </p>
+            <Link to={<SignUp/>}/>
+        </span>
+        
     </div>
 )
 }
