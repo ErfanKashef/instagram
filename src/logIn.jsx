c5bdaee471b64c8abfb733b0b447bed668ff90e1
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-13 10:07:49 +0330

    ✨ insta logo added to login

--- src/logIn.jsx
+++ src/logIn.jsx
@@ -1,8 +1,9 @@
+import { InstaTitle } from "./InstaTitle";
 
-export default function LogIn() {
+export function LogIn() {
     return (
     <div>
-        
+        <InstaTitle LogoWidth="255px" LogoHeight="87px"/>
     </div>
 )
 }
