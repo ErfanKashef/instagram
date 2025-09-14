8070cdf824f65badd6250ac2348a5dc8c4858f56
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-14 09:49:27 +0330

    🎉 api fetched

--- src/components/page/myprofile.jsx
+++ src/components/page/myprofile.jsx
@@ -2,8 +2,24 @@ import ProfileImage from "./myProfileImage";
 import ProfileStats from "./myProfileStats";
 import ProfileInfo from "./myProfileInfo";
 import ProfileGallery from "./myProfileGallery";
+import { useEffect } from "react";
+import { clinet } from "../../lib";
 
 const MyProfile = () => {
+
+  useEffect (() => {
+    const followers = async () => {
+      try {
+        response = clinet.get(`/api/user/followers/${username}`)
+        console.log(response)
+      }
+      catch (err) {
+        console.log(err)
+      }
+    }
+    followers()
+  }, [username])
+
   return (
     <div className="container mx-auto">
       <div className="flex justify-center gap-20 py-10">
